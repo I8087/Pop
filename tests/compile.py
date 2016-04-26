5974158bfd344ecf9aa4b70bf10c953fb07c2405
I8087 <i8086@protonmail.com>
2016-04-25 22:32:06 -0400

    changed ./test to ./tests

--- tests/compile.py
+++ tests/compile.py
@@ -7,11 +7,11 @@ import os, sys
 os.chdir("\\".join(sys.argv[0].split("\\")[:-2]))
 
 # Get a list of test*.pop files.
-popfiles = [i[:-4] for i in os.listdir("test") if i.startswith("test") and i.endswith(".pop")]
+popfiles = [i[:-4] for i in os.listdir("tests") if i.startswith("test") and i.endswith(".pop")]
 
 # Build each pop file.
 for i in popfiles:
-    os.system("py -3.4 -m pop -d test -o build\\{0}.exe {0}.pop".format(i))
+    os.system("py -3.4 -m pop -d tests -o build\\{0}.exe {0}.pop".format(i))
 
 # Pause when we're done, just for good measure!
 os.system("pause")
